0f284c9218472e9135c5249829f93f7268dd9293
joseap <josxe89@gmail.com>
2022-01-30 04:20:27 -0600

    version  responsiva del index

---
+++ front/styles/styledComponents/StyledIndex.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import Theater from '../../public/images/movie-theater.jpg';
+
+const StyledIndex = styled.div`
+
+    #form-container{
+
+        .select-react {
+        width: 280px;
+        }
+
+        .form-control.form-control.form-control {
+            width: 180px;
+        }
+
+        #form-container {
+            color: rgb(230, 230, 230);
+            width: 100vw;
+            padding: 40px 50px 40px 50px;
+        }
+
+        /* grid para los campos de crear la nueva cuenta*/
+
+        .forma-crear-cuenta{
+            display: grid;
+            grid-template-columns: 1fr 1fr 1fr;
+        }
+        @media (max-width: 950px) {
+            #form-container {
+                width: 100vw;
+                padding: 40px 20px 40px 20px;
+            }
+            .forma-crear-cuenta{
+                display: grid;
+                grid-template-columns: 1fr 1fr;
+                grid-column: 10px;
+            }
+        }
+
+    }
+
+
+`;
+
+export default StyledIndex; 
